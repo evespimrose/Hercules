61311b3118eaba50823cf97e4b297ebae7691c2e
evespimrose <evespimrose@users.noreply.github.com>
2025-08-13 03:53:11 +0900

    [Feat] 싱글톤 인스턴스 정의

--- Assets/Scripts/Singletons/Singleton.cs
+++ Assets/Scripts/Singletons/Singleton.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
